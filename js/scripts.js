af672353527e9aefca1fbdc109ab311c0a4e3b38
Duncan Robbins <duncanrobbins19@gmail.com>
2019-08-23 14:34:42 -0700

    start adding the javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,52 @@
+//back end
+var romanNumber = [];
+
+
+function toRoman(inputNumber) {
+  for (var i = inputNumber; i >= 0 ; i+=0) {
+    console.log(i);
+    if (i = 3) {
+      romanNumber.push("i'm sorry, Dave I'm afraid I can't do that.");
+      var i = (i - 1);
+        var inputNumber = i;
+    } else if (i = 2) {
+      romanNumber.push("Boop!");
+      var i = (i - 1);
+        var inputNumber = i;
+    } else if (i = 1) {
+      romanNumber.push("Beep!");
+      var i = (i-1);
+        var inputNumber = i;
+      } else {
+      romanNumber.push(i);
+      var i = (i-1);
+        var inputNumber = i;
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
   $("form#numberForm").submit(function(){
     event.preventDefault();
