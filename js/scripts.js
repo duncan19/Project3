48e9e28cb7c0374806a3d5e30699161ec898140c
Duncan Robbins <duncanrobbins19@gmail.com>
2019-08-23 16:04:09 -0700

    add more java script to the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 //back end
-var romanNumber = [];
+var listofnumbers = [];
 
 
-function toRoman(inputNumber) {
+function toList(inputNumber) {
   for (var i = inputNumber; i >= 0 ; i+=0) {
     console.log(i);
     if (i = 3) {
-      romanNumber.push("i'm sorry, Dave I'm afraid I can't do that.");
+      listofnumbers.push("i'm sorry, Dave I'm afraid I can't do that.");
       var i = (i - 1);
         var inputNumber = i;
     } else if (i = 2) {
-      romanNumber.push("Boop!");
+      listofnumbers.push("Boop!");
       var i = (i - 1);
         var inputNumber = i;
     } else if (i = 1) {
-      romanNumber.push("Beep!");
+      listofnumbers.push("Beep!");
       var i = (i-1);
         var inputNumber = i;
       } else {
-      romanNumber.push(i);
+      listofnumbers.push(i);
       var i = (i-1);
         var inputNumber = i;
       }
@@ -30,10 +30,10 @@ $(document).ready(function(){
   $("form#numberForm").submit(function(){
     event.preventDefault();
     var inputNumber = parseInt($("input[name=numberInput]").val());
-    toRoman(inputNumber);
-    romanNumber = romanNumber.join("")
+    toList(inputNumber);
+    listofnumbers = listofnumbers.join("")
 
-    console.log(romanNumber);
-    $("p#results").text(romanNumber);
+    console.log(listofnumbers);
+    $("p#results").text(listofnumbers);
   });
 });
